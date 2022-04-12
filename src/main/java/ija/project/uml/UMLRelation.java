053ed2e7f28dfa7b56a4f2f495d6b18fe7324ca7
Lukáš Vincenc <lucky.vincenc@gmail.com>
2022-04-12 21:18:20 +0200

    filled UMLRelation class

--- src/main/java/ija/project/uml/UMLRelation.java
+++ src/main/java/ija/project/uml/UMLRelation.java
@@ -1,4 +1,45 @@
 package ija.project.uml;
 
-public class UMLRelation {
+public class UMLRelation extends Element {
+
+    private final String type;
+    private final UMLClass source;
+    private final UMLClass target;
+    private final String description;
+    private final String cardinalityFrom;
+    private final String cardinalityTo;
+
+    public UMLRelation (String name, String type, UMLClass source, UMLClass target, String description, String cardinalityFrom, String cardinalityTo) {
+        super(name);
+        this.type = type;
+        this.source = source;
+        this.target = target;
+        this.description = description;
+        this.cardinalityFrom = cardinalityFrom;
+        this.cardinalityTo = cardinalityTo;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public UMLClass getSource() {
+        return source;
+    }
+
+    public UMLClass getTarget() {
+        return target;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCardinalityFrom() {
+        return cardinalityFrom;
+    }
+
+    public String getCardinalityTo() {
+        return cardinalityTo;
+    }
 }
