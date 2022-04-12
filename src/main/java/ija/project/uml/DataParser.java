284506b07e688e216306658040b7394469a6dea4
achxkloel <evgeny.torbin@gmail.com>
2022-04-12 18:53:58 +0200

    add parse json method, add test json files

---
+++ src/main/java/ija/project/uml/DataParser.java
@@ -0,0 +1,35 @@
+package ija.project.uml;
+
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+/**
+ * Parse JSON data.
+ */
+public class DataParser {
+    /**
+     * JSON Object.
+     */
+    private JSONObject jsonObj;
+
+    /**
+     * Creates an empty JSON object.
+     */
+    public DataParser () {
+        this.jsonObj = new JSONObject();
+    }
+
+    /**
+     * Parse input data file and save json object.
+     * 
+     * @param dataFile input data file.
+     * @throws IOException
+     */
+    public void parse (File dataFile) throws IOException {
+        String jsonData = Files.readString(dataFile.toPath());
+        this.jsonObj = new JSONObject(jsonData);
+    }
+}
