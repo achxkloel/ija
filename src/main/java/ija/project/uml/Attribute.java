1c83156675702435c058b2981d1a1657d40c863a
Lukáš Vincenc <lucky.vincenc@gmail.com>
2022-04-12 21:06:04 +0200

    filled Attribute class and Method class

--- src/main/java/ija/project/uml/Attribute.java
+++ src/main/java/ija/project/uml/Attribute.java
@@ -1,4 +1,20 @@
 package ija.project.uml;
 
-public class Attribute {
+public class Attribute extends Element {
+
+    private final String type;
+
+    /**
+     * Creates new attribute
+     * @param name name of attribute
+     */
+    public Attribute (String name, String type) {
+        super(name);
+        this.type = type;
+    }
+
+    public String getType () {
+        return type;
+    }
+
 }
--- src/main/java/ija/project/uml/Method.java
+++ src/main/java/ija/project/uml/Method.java
@@ -1,4 +1,43 @@
 package ija.project.uml;
 
-public class Method {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Method extends Attribute {
+
+    /**
+     * Return type of method
+     */
+    private final String returnType;
+    /**
+     * List of attributes of method
+     */
+    private final List<Attribute> attributeList = new ArrayList<>();
+
+    /**
+     * Creates new method
+     * @param name name of method
+     */
+    public Method (String name, String type, String returnType) {
+        super(name, type);
+        this.returnType = returnType;
+    }
+
+    /**
+     * Get return type of Method
+     * @return return type of method
+     */
+    public String getReturnType () {
+        return returnType;
+    }
+
+    /**
+     * Add new attribute to method.
+     *
+     * @param newAttribute new attribute
+     */
+    public void addAttribute (Attribute newAttribute) {
+        attributeList.add(newAttribute);
+    }
+
 }
