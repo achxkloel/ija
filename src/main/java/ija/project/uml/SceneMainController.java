8ef786f695860cc959c050431115c9299451ac5d
Lukáš Vincenc <lucky.vincenc@gmail.com>
2022-05-06 17:58:38 +0200

    SceneMainController cleanup - divided into separate methods

--- src/main/java/ija/project/uml/SceneMainController.java
+++ src/main/java/ija/project/uml/SceneMainController.java
@@ -27,45 +27,67 @@ public class SceneMainController {
     double orgSceneX, orgSceneY;
     double orgTranslateX, orgTranslateY;
 
-    /**
-     * method to display a string, that was passed to it
-     * @param result the displayed string
-     */
-    public void displayResult(String result) {
-        final VBox vbox = new VBox();
-        final VBox nestedVboxAttributes = new VBox();
-        final VBox nestedVboxMethods = new VBox();
-
+    private Label getLabel(String text) {
         Label label = new Label();
-        label.setText("Test label");
-        Text attributes = new Text();
-        attributes.setText("Test attribute 1\nTest attribute 2");
-        Text methods = new Text();
-        methods.setText("Test method 1\nTest method 2\nTest method 3");
+        label.setText(text);
 
         String cssLabel = "-fx-text-fill: black;\n" +
                 "-fx-padding: 5px";
 
-        String cssLayoutVbox = "-fx-border-color: black;\n" +
-                "-fx-border-width: 2;\n" +
-                "-fx-border-style: solid;\n" +
-                "-fx-background-color: white;\n";
+        label.setStyle(cssLabel);
+
+        return label;
+    }
+
+    private VBox getAttributes(String text) {
+        Text attributes = new Text();
+        attributes.setText(text);
+
+        final VBox vboxAttributes = new VBox();
+        vboxAttributes.getChildren().add(attributes);
 
         String cssLayoutNestedVbox = "-fx-border-color: black;\n" +
                 "-fx-border-width: 2;\n" +
                 "-fx-border-style: solid none none none;\n" +
                 "-fx-background-color: white;\n" +
                 "-fx-padding: 3px";
+        vboxAttributes.setStyle(cssLayoutNestedVbox);
+
+        return vboxAttributes;
+    }
+
+    private VBox getMethods(String text) {
+        final VBox vboxMethods = new VBox();
+
+        Text methods = new Text();
+        methods.setText(text);
+        String cssLayoutNestedVbox = "-fx-border-color: black;\n" +
+                "-fx-border-width: 2;\n" +
+                "-fx-border-style: solid none none none;\n" +
+                "-fx-background-color: white;\n" +
+                "-fx-padding: 3px";
+
+        vboxMethods.setStyle(cssLayoutNestedVbox);
+        vboxMethods.getChildren().add(methods);
+
+        return vboxMethods;
+    }
+
+    /**
+     * method to display a string, that was passed to it
+     * @param result the displayed string
+     */
+    public void displayResult(String result) {
+        final VBox vbox = new VBox();
+        String cssLayoutVbox = "-fx-border-color: black;\n" +
+                "-fx-border-width: 2;\n" +
+                "-fx-border-style: solid;\n" +
+                "-fx-background-color: white;\n";
 
         vbox.setStyle(cssLayoutVbox);
-        label.setStyle(cssLabel);
-        nestedVboxAttributes.setStyle(cssLayoutNestedVbox);
-        nestedVboxMethods.setStyle(cssLayoutNestedVbox);
-        vbox.getChildren().add(label);
-        vbox.getChildren().add(nestedVboxAttributes);
-        vbox.getChildren().add(nestedVboxMethods);
-        nestedVboxAttributes.getChildren().add(attributes);
-        nestedVboxMethods.getChildren().add(methods);
+        vbox.getChildren().add(getLabel("Test label"));
+        vbox.getChildren().add(getAttributes("Test attribute 1\nTest attribute 2"));
+        vbox.getChildren().add(getMethods("Test method 1\nTest method 2\nTest method 3"));
 
         vbox.setOnMousePressed(vboxOnMousePressedEventHandler);
         vbox.setOnMouseDragged(vboxOnMouseDraggedEventHandler);
