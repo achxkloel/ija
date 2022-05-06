dea63338dbaddb9bc93e539f2866f9be83a86c48
Lukáš Vincenc <lucky.vincenc@gmail.com>
2022-05-06 12:55:26 +0200

    added draggable circle

--- src/main/java/ija/project/uml/SceneWelcomeController.java
+++ src/main/java/ija/project/uml/SceneWelcomeController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
