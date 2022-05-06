3c51613959b5f857d5162eca7c07182fa79b2ccb
achxkloel <evgeny.torbin@gmail.com>
2022-05-06 22:29:33 +0200

    remove useless imports

--- src/main/java/ija/project/uml/SceneEditUMLClassController.java
+++ src/main/java/ija/project/uml/SceneEditUMLClassController.java
@@ -11,20 +11,10 @@ package ija.project.uml;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.stage.Window;
-
-import java.io.IOException;
 
 /**
  * Controller for the editUMLClass scene.
